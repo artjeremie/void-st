23962b7493df3ba5bde02aa38e459b6a0c1543a0
hoaxdream <hoaxdream@gmail.com>
2022-01-18 04:48:58 +0800

    Minor edit

--- config.def.h
+++ config.def.h
@@ -126,7 +126,7 @@ static const char *colorname[] = {
 
     /* more colors can be added after 255 to use with DefaultXX */
     "#add8e6", /* 256 -> cursor */
-    "#555555", /* 257 -> rev cursor*/
+    "#d8dee9", /* 257 -> rev cursor*/
     "#1d2021", /* background */
     "#ebdbb2", /* foreground */
 };
